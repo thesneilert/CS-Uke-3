f516c3185a18130da5b275cf7881c4d9f8d06d08
thesneilert <eilert.gaming@outlook.com>
2023-05-21 18:51:58 +0200

    Finished!

--- Ukesoppgave Uke 3/Ukesoppgave Uke 3/Program.cs
+++ Ukesoppgave Uke 3/Ukesoppgave Uke 3/Program.cs
@@ -2,11 +2,11 @@
 
 internal class Program
 {
-    
-    
-    private static List<Pet> pet = new()
+    private static readonly List<Pet> pets = new()
     {
-        new Pet("arne", "katt", 20, 10, 1)
+        new Pet("Luna", "Katt", 3, 5, 2, "tunfisk"),
+        new Pet("Balder", "Hund", 6, 8, 3, "kjøttboller"),
+        new Pet("Nemo", "Fisk", 1, 3, 1, "algemix")
     };
 
     private static void Main(string[] args)
@@ -17,30 +17,75 @@ internal class Program
 
     public static void MainMenu()
     {
-        Console.WriteLine("Trykk 1");
-        var menyChoose = Console.ReadLine();
+        Console.WriteLine("Velg et kjæledyr:");
+        Console.WriteLine("1. Luna (Katt)");
+        Console.WriteLine("2. Balder (Hund)");
+        Console.WriteLine("3. Nemo (Fisk)");
+        var petChoice = Console.ReadLine();
 
-        if (menyChoose == "1")
+        if (petChoice == "1" || petChoice == "2" || petChoice == "3")
         {
-            
+            var choice = Convert.ToInt32(petChoice);
+            var pet = pets[choice - 1];
+            PetMenu(pet);
         }
         else
         {
             Console.WriteLine("Ugyldig valg. Prøv igjen.");
             Console.Clear();
+            MainMenu();
+        }
+    }
+
+    public static void PetMenu(Pet pet)
+    {
+        Console.WriteLine($"Du har valgt {pet.Name}, en {pet.Type}.");
+        Console.WriteLine("Velg en handling:");
+        Console.WriteLine("1. Mate");
+        Console.WriteLine("2. Leke");
+        var actionChoice = Console.ReadLine();
+
+        if (actionChoice == "1")
+        {
+            Console.WriteLine("Velg mat:");
+            Console.WriteLine("1. Tunfisk");
+            Console.WriteLine("2. Kjøttboller");
+            Console.WriteLine("3. Algemix");
+            var foodChoice = Console.ReadLine();
+            var food = GetFoodChoice(foodChoice);
+            pet.Eat(food);
+        }
+        else if (actionChoice == "2")
+        {
+            pet.Play();
+            Console.WriteLine($"Du lekte med {pet.Name}. Lykkeligheten har økt.");
+        }
+        else
+        {
+            Console.WriteLine("Ugyldig valg. Prøv igjen.");
+        }
+
+        Console.WriteLine($"Sultnivå: {pet.Hunger}");
+        Console.WriteLine($"Lykkelighetsnivå: {pet.Happiness}");
+
+        Console.WriteLine("Trykk en tast for å fortsette...");
+        Console.ReadKey();
+        Console.Clear();
+        MainMenu();
+    }
+
+    public static string GetFoodChoice(string foodChoice)
+    {
+        switch (foodChoice)
+        {
+            case "1":
+                return "tunfisk";
+            case "2":
+                return "kjøttboller";
+            case "3":
+                return "algemix";
+            default:
+                return "";
         }
     }
 }
-
-// public static void MainMenu2()
-// {
-//     Console.WriteLine("gjør dyret glad");
-//     Console.WriteLine();
-//     pet[0].happyPlus();
-//     Console.WriteLine($"{pet[0].Happiness}");
-//         
-//     Console.WriteLine("bytt navn:");
-//     var navn = Console.ReadLine();
-//     pet[0].changeName(navn);
-//     Console.WriteLine($"{pet[0].Name}");
-// }
--- Ukesoppgave Uke 3/Ukesoppgave Uke 3/pets.cs
+++ Ukesoppgave Uke 3/Ukesoppgave Uke 3/pets.cs
@@ -1,57 +1,42 @@
-namespace Ukesoppgave_Uke_3;
+namespace Ukesoppgave_Uke_3
 
-public class Pet
-{
-    public string Name { get; private set; }
-    public string Type { get; private set; }
-    public int Age { get; private set; }
-    public int Happiness { get; private set; }
-    public int Hunger { get; private set; }
-    
-    public Pet(string name, string type, int age, int happiness, int hunger)
     {
-        Name = name;
-        Type = type;
-        Age = age;
-        Happiness = happiness;
-        Hunger = hunger;
-    }
-
-    public void eat()
-    {
-        if ()
+        public class Pet
         {
-        Hunger += 1
-        }
-        else ()
-        {
-        Hunger += 2    
+            public string Name { get; private set; }
+            public string Type { get; private set; }
+            public int Age { get; private set; }
+            public int Happiness { get; private set; }
+            public int Hunger { get; private set; }
+            private readonly string favoriteFood;
+
+            public Pet(string name, string type, int age, int happiness, int hunger, string favoriteFood)
+            {
+                Name = name;
+                Type = type;
+                Age = age;
+                Happiness = happiness;
+                Hunger = hunger;
+                this.favoriteFood = favoriteFood;
+            }
+
+            public void Eat(string food)
+            {
+                if (food == favoriteFood)
+                {
+                    Hunger += 1;
+                    Console.WriteLine($"Hurra! Dette er det beste jeg vet. Tusen takk for {food}!");
+                }
+                else
+                {
+                    Hunger += 2;
+                    Console.WriteLine($"Takk for {food}!");
+                }
+            }
+
+            public void Play()
+            {
+                Happiness += 1;
+            }
         }
     }
-
-    public void play()
-    {
-        Happiness += 1
-    }
-}
-
-//     public void happyPlus()
-//     {
-//         Happiness++;
-//     }
-//
-//     public void changeName(string x)
-//     {
-//         Name = x;
-//     }
-
-//     public void kjørcmd(string cmd)
-//     {
-//         switch (cmd)
-//         {
-//             case"lek":
-//                 Console.WriteLine("leker");
-//                 break;
-//         }
-//     }
-
